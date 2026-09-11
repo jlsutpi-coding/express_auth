97d8c80e7789cb3fe702deeb38cf7fb1880eef58
jlsutpi-coding <sutpi070@gmail.com>
2026-09-11 22:13:59 +0700

    implement user route with express router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import express, { type Request, type Response } from "express";
 
-import {
-  query,
-  validationResult,
-  matchedData,
-  checkSchema,
-} from "express-validator";
+import { query, validationResult } from "express-validator";
 
-import { createUserValidationSchema } from "./util/validationSchema";
+import router from "./routes/user";
 
 const app = express();
 
@@ -15,37 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use("/api/users", router);
+
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "Hello, World!" });
 });
 
-app.get(
-  "/api/users",
-  query("filter").isString().notEmpty(),
-  (req: Request<{ filter: string }>, res: Response) => {
-    const { filter } = req.query;
-
-    const results = validationResult(req);
-    console.log("Validation results:", results);
-    return res.status(200).send({ msg: `Filter applied: ${filter}` });
-  },
-);
-
-app.post(
-  "/api/users",
-  checkSchema(createUserValidationSchema),
-
-  (req: Request, res: Response) => {
-    const results = validationResult(req);
-    if (!results.isEmpty()) {
-      return res.status(400).json({ errors: results.array() });
-    }
-
-    const data = matchedData(req);
-    const { username, email } = data;
-    return res.status(201).send({ msg: `User created: ${username}, ${email}` });
-  },
-);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ src/routes/user.ts
@@ -0,0 +1,44 @@
+import express, { type Request, type Response } from "express";
+import { createUserValidationSchema } from "../util/validationSchema";
+import {
+  checkSchema,
+  matchedData,
+  validationResult,
+  query,
+} from "express-validator";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ msg: "User route is working!" });
+});
+
+router.get(
+  "",
+
+  query("filter").isString().notEmpty(),
+  (req: Request<{ filter: string }>, res: Response) => {
+    const { filter } = req.query;
+
+    const results = validationResult(req);
+    console.log("Validation results:", results);
+    return res.status(200).send({ msg: `Filter applied: ${filter}` });
+  },
+);
+
+router.post(
+  "",
+  checkSchema(createUserValidationSchema),
+
+  (req: Request, res: Response) => {
+    const results = validationResult(req);
+    if (!results.isEmpty()) {
+      return res.status(400).json({ errors: results.array() });
+    }
+
+    const data = matchedData(req);
+    const { username, email } = data;
+    return res.status(201).send({ msg: `User created: ${username}, ${email}` });
+  },
+);
+export default router;
